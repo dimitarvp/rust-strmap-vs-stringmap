79a61ed3f15bb2ac634770ebf9c3bedf5c852815
Dimitar Panayotov <mitko.p@gmail.com>
2021-03-04 00:54:15 +0200

    Add 32 hardcoded paths for insert benchmarks

--- benches/strmap_vs_stringmap.rs
+++ benches/strmap_vs_stringmap.rs
@@ -1,6 +1,41 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::collections::HashMap;
 
+static KEYS: [&str; 32] = [
+    "/usr/olivia/etc/woman.csv",
+    "/bin/week/company/var/mia/elijah.rar",
+    "/var/hand/different/usr/group/company.ppt",
+    "/var/same/logan/little/public.flv",
+    "/home/lucas/sophia/large/able.doc",
+    "/sbin/mason/high/next/company/usr/great/same/first.jpg",
+    "/tmp/large/man/tmp/year/liam/mason.tar",
+    "/bin/big/var/person.png",
+    "/var/large/sophia/aria/jackson/sbin/james/important/number.doc",
+    "/usr/sbin/same/government/first.csv",
+    "/home/next/able/etc/oliver/man.csv",
+    "//life.zip",
+    "/var/lucas/different/usr/isabella/large.mp3",
+    "/long/different/etc/way/lucas.so",
+    "/sbin/part/number/usr/next.mp3",
+    "/time/lucas/public/var/few/logan/old.rar",
+    "/etc/way/eye/tmp/thing.jpg",
+    "/etc/liam/bad/new/sbin/child/company.ppt",
+    "/sbin/part/jackson/var/place/noah.xls",
+    "/usr/little/eye/old/group/etc.ppt",
+    "/var/high/work/home/group/important.rar",
+    "/sbin/last/noah/long/sbin.zip",
+    "/bin/company/var/case.txt",
+    "/group/bin/james/public/emma.doc",
+    "/var/liam/var/mason.ppt",
+    "//usr/olivia/own.csv",
+    "/etc/emma/year/home/life/man.zip",
+    "/etc/last/home/aria/eye.zip",
+    "/usr/elijah/home/point/few/week/place.rar",
+    "/home/oliver/amelia/same/usr.doc",
+    "/var/oliver/fact/elijah/sbin/small/emma/other/place.tar",
+    "/home/woman/charlotte/sbin/own/case.mp4",
+];
+
 fn insert_str<'a>(map: &mut HashMap<&'a str, usize>, key: &'a str) {
     map.insert(key, 0);
 }
@@ -10,31 +45,14 @@ fn insert_string<T: Into<String>>(map: &mut HashMap<String, usize>, key: T) {
 }
 
 fn test_insert_str() {
-    let k0 = "$HOME/1.file";
-    let k1 = "~/2.file";
-    let k2 = "/Users/somebody/3.file";
-    let k3 = String::from("/home/somebody/someplace/4.file");
-    let k4 = String::from("/path/to/somewhere/5.file");
     let mut map = HashMap::<&str, usize>::with_capacity(32);
-    insert_str(&mut map, k0);
-    insert_str(&mut map, k1);
-    insert_str(&mut map, k2);
-    insert_str(&mut map, &k3);
-    insert_str(&mut map, &k4);
+    KEYS.iter().for_each(|k| insert_str(&mut map, k));
 }
 
 fn test_insert_string() {
-    let k0 = "$HOME/1.file";
-    let k1 = "~/2.file";
-    let k2 = "/Users/somebody/3.file";
-    let k3 = String::from("/home/somebody/someplace/4.file");
-    let k4 = String::from("/path/to/somewhere/5.file");
     let mut map = HashMap::<String, usize>::with_capacity(32);
-    insert_string(&mut map, k0);
-    insert_string(&mut map, k1);
-    insert_string(&mut map, k2);
-    insert_string(&mut map, k3);
-    insert_string(&mut map, k4);
+    KEYS.iter()
+        .for_each(|k| insert_string(&mut map, k.to_string()));
 }
 
 fn benchmark_insert_str(c: &mut Criterion) {
